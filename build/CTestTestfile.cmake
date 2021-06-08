7cabc878b3ae3bd2e5588920669f4a8941e52ef0
kim <kimanan555@hotmail.com>
2021-06-08 11:59:17 +0700

    project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kim/ws/src
+# Build directory: /home/kim/ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
